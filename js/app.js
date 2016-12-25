615ae600487fcaf100cab0fa1175877c58820e2a
Arseni <arsenybor@gmail.com>
2016-12-24 19:26:53 -0800

    added empty portfolio div

--- js/app.js
+++ js/app.js
@@ -62,7 +62,7 @@ particlesJS('particles-js',
     },
     "move": {
       "enable": true,
-      "speed": 3,
+      "speed": 2,
       "direction": "bottom",
       "random": false,
       "straight": true,
